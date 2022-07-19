f90aa71d261b5d3a666863bd98537d65974886ce
Alex <hambiru@yandex.ru>
2022-07-19 10:13:20 +0300

    send on key 'Enter', Onclick avatar profile

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -5,11 +5,13 @@ import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-import { Button, Stack, TextField } from '@mui/material';
+import { Box, Button, Stack, TextField } from '@mui/material';
 import { Container } from '@mui/system';
 import { useAppDispatch, useAppSelector } from '../../store/store';
 
 import { actions, sendMessage, startMessagesListening, stopMessagesListening } from '../../store/reducers/chatReducer';
+import { Link, Navigate } from 'react-router-dom';
+import { authApi } from '../../store/api/authApi';
 
 
 export type ChatMessageType = {
@@ -21,25 +23,27 @@ export type ChatMessageType = {
 
 const ChatPage: React.FC = React.memo(() => {
   const dispatch = useAppDispatch();
+  const { data: authData } = authApi.useGetIsAuthorizedQuery();
   const messagesAnchorRef = React.useRef<HTMLDivElement>(null);
   const { messages, status } = useAppSelector(state => state.chatReducer);
   const [message, setMessage] = React.useState('');
   const [isAutoScroll, setIsAutoScroll] = React.useState(true);
+  
+  const maxMessageLength = message.length > 100;
+  
 
-
-  let newMesages = messages;
   React.useEffect(() => {
     dispatch(startMessagesListening());
     return () => {
       dispatch(stopMessagesListening());
       dispatch(actions.removeMessages());
-      
+
     }
   }, [])
 
   React.useEffect(() => {
     if (isAutoScroll) {
-      messagesAnchorRef.current?.scrollIntoView({block: "start", inline: "nearest", behavior: 'smooth'})
+      messagesAnchorRef.current?.scrollIntoView({ block: "start", inline: "nearest", behavior: 'smooth' })
     }
   }, [messages])
 
@@ -48,7 +52,9 @@ const ChatPage: React.FC = React.memo(() => {
     setMessage(e.target.value)
   }
 
-  const handleClick = () => {
+  const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
     if (!message) {
       return
     }
@@ -64,56 +70,71 @@ const ChatPage: React.FC = React.memo(() => {
       isAutoScroll && setIsAutoScroll(false)
     }
   }
-  
-  console.log('rendered ChatPage');
-  console.log(messages);
-  console.log(newMesages);
-
 
 
   return (
     <>
-      {/* {!data?.id ? 
-    <Navigate to='/login'/>
-    :  */}
-      {status === 'error' && <div>Some error occured. Please refresh the page</div>}
+      {authData !== undefined && !authData?.id ?
+        <Navigate to='/login' />
+        :
+        <Container maxWidth="xl">
+          {status === 'error' && <div>Some error occured. Please refresh the page</div>}
+          <List
+            sx={{ width: '100%', maxWidth: 1000, height: '100%', maxHeight: 500, bgcolor: 'background.paper', overflow: 'auto' }}
+            onScroll={scrollHandler}
+          >
+            {messages.map((message, index) => (
+              <div key={message.id}>
+                <ListItem alignItems="flex-start">
+                  <Link to={'/profile/' + message.userId}>
+                    <ListItemAvatar>
+                      <Avatar alt="UserAvatar" src={message.photo} />
+                    </ListItemAvatar>
+                  </Link>
+                  <ListItemText
+                    primary={message.userName}
+                    secondary={
+                      <React.Fragment>
+                        {message.message}
+                      </React.Fragment>
+                    }
+                  />
+                </ListItem>
+                <Divider variant="inset" component="li" />
+              </div>
+            ))}
+            <div ref={messagesAnchorRef}></div>
 
-      <Container maxWidth="xl">
-        <List
-          sx={{ width: '100%', maxWidth: 1000, height: '100%', maxHeight: 500, bgcolor: 'background.paper', overflow: 'auto' }}
-          onScroll={scrollHandler}
-        >
-          {messages.map((message, index) => (
-            <div key={message.id}>
-              <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                  <Avatar alt="UserAvatar" src={message.photo} />
-                </ListItemAvatar>
-                <ListItemText
-                  primary={message.userName}
-                  secondary={
-                    <React.Fragment>
-                      {message.message}
-                    </React.Fragment>
-                  }
-                />
-              </ListItem>
-              <Divider variant="inset" component="li" />
-            </div>
-          ))}
-          <div ref={messagesAnchorRef}></div>
+          </List>
 
-        </List>
-        <Stack spacing={3} direction="row" sx={{ marginTop: 5 }}>
-          <TextField id="outlined-basic" label="New message" variant="outlined" value={message} onChange={handleChange} />
-          <Button disabled={status !== 'ready'} onClick={handleClick} variant="outlined">Send message</Button>
-        </Stack>
-      </Container>
+          <Box sx={{ marginTop: 5 }} component="form" onSubmit={onFormSubmit}>
+            <TextField
+              sx={{ width: '50%' }}
+              id="outlined-basic"
+
+              label="New message"
+              variant="outlined"
+              value={message}
+              onChange={handleChange}
+              size="small"
+              error = {maxMessageLength}
+              helperText={maxMessageLength && "Must be 100 characters or less"}
+            />
+            <Button
+              sx={{ marginLeft: 2 }}
+              type="submit"
+              disabled={status !== 'ready' || maxMessageLength}
+              variant="outlined"
+            >
+              Send
+            </Button>
+          </ Box>
+        </Container>
 
 
 
 
-      {/* } */}
+      }
 
     </>
 
